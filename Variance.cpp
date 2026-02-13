b10b1a790ff261c4674beee9b80b44aa8e0cb453
Pap Áron <paronka04@gmail.com>
2026-02-13 23:30:13 +0100

    Update: Variance.cpp, variation calculation now has it's own method

--- Variance.cpp
+++ Variance.cpp
@@ -2,24 +2,29 @@ using namespace std;
 #include <iostream>
 #include <vector>
 
-double expected_value_calculation(vector<double> X);
+double expected_value(vector<double> X);
+double variance(vector<double> X);
 
 int main() {
     vector<double> X = {2,4,9};
-    double Xexp = expected_value_calculation(X);
+    double solution = variance(X);
+    cout << "The variance for the input is: " << solution << endl;
+    return 0;
+}
+
+double variance(vector<double> X) {
+    double Xexp = expected_value(X);
     vector<double> Xsqr;
     for (int i = 0; i < X.size(); i++) {
         Xsqr.push_back(X[i]*X[i]);
     }
-    double Xsqrexp = expected_value_calculation(Xsqr);
     
-    double variance = Xsqrexp - (Xexp * Xexp);
-    cout << "The variance for the input is: " << variance << endl;
-
-    return 0;
+    double Xsqrexp = expected_value(Xsqr);
+    double solution = Xsqrexp - (Xexp * Xexp);
+    return solution;
 }
 
-double expected_value_calculation(vector<double> X) {
+double expected_value(vector<double> X) {
     double sumX = 0;
     for (int i = 0; i < X.size(); i++) {
         sumX += X[i];
